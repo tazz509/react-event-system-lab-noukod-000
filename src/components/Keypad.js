77f369b5d7217cd8b67736f6920c7fa77053915d
carly <carly@noukod.com>
2019-03-29 15:40:08 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,10 +1,10 @@
 // Code Keypad Component Here
 class Tickler extends React.Component {
- 
+
   tickle = () => {
     console.log('Tee hee!');
   }
- 
+
   render() {
     return (
       <button onClick={this.tickle}>Tickle me!</button>
